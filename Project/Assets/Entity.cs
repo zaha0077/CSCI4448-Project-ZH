ac72c541736ee1286d4f20f60a82fe3017a04072
zaha0077 <zaha0077@colorado.edu>
2018-11-01 00:36:48 -0600

    slight edit

--- Project/Assets/Entity.cs
+++ Project/Assets/Entity.cs
@@ -12,18 +12,13 @@ public class Entity : MonoBehaviour {
 		return speed_;
 	}
 	public virtual void Move(){
-		Vector3 temp = this.transform.position;
-		temp.x += speed_;
-		this.transform.position = temp;
 	}
 
 	// Use this for initialization
 	void Start () {
-		setSpeed (0.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Move();
 	}
 }

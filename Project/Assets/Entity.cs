944cffb012cd4a1bfddc8954cc7dcbca86b28993
zaha0077 <zaha0077@colorado.edu>
2018-10-31 18:37:18 -0600

    Add files via upload

---
+++ Project/Assets/Entity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+	private float speed_;
+
+	public void setSpeed(float value){
+		speed_ = value;
+	}
+	public float getSpeed(){
+		return speed_;
+	}
+	public void Move(){
+		Vector3 temp = this.transform.position;
+		temp.x += speed_;
+		this.transform.position = temp;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+}
